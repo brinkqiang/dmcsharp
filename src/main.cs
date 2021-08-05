97cf6ed845f9636d241b881583e25172340b7ac8
brinkqiang <brink.qiang@gmail.com>
2021-08-05 14:38:34 +0800

    add

--- src/main.cs
+++ src/main.cs
@@ -33,33 +33,21 @@ namespace HelloWorldApplication //namespace声明命名空间,包含一个hello
 
             Console.WriteLine(strOut2);
 
+            Db.fd_task task = new Db.fd_task
+            {
+                TaskId = 1001,
+                Count = 1,
+                State = 1,
+            };
 
-            //Db.fd_task task = new Db.fd_task
-            //{
-            //    TaskId = 1001,
-            //    Count = 1,
-            //    State = 1,
-            //};
+            byte[] buffer = task.ToByteArray();
 
-            //byte[] buffer = task.ToByteArray();
+            oArc4.Encrypt(buffer, 0, buffer, 0, buffer.Length);
+            oArc4.Decrypt(buffer, 0, buffer, 0, buffer.Length);
 
-            //Db.fd_task p = Db.fd_task.Parser.ParseFrom(buffer);
+            Db.fd_task copyTask = Db.fd_task.Parser.ParseFrom(buffer);
 
-            //CDMRC oArc4 = new CDMRC();
-            //oArc4.SetKey("1234");
-
-            //oArc4.Encrypt(buffer, 0, buffer, 0, buffer.Length);
-            //oArc4.Decrypt(buffer, 0, buffer, 0, buffer.Length);
-
-            //string strOut = System.Text.Encoding.ASCII.GetString(buffer);
-            //Console.WriteLine(strOut);
-
-            //oArc4.Encrypt(buffer);
-            //oArc4.Decrypt(buffer);
-
-            //Db.fd_task p2 = Db.fd_task.Parser.ParseFrom(buffer);
-
-            //Console.WriteLine(p2.ToString());
+            Console.WriteLine(copyTask.ToString());
         }
     }
 }
