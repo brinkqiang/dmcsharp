cc8d1718138514fe346314b9e6735b3b0a86f99b
brinkqiang <brink.qiang@gmail.com>
2021-08-05 14:30:06 +0800

    add

--- src/main.cs
+++ src/main.cs
@@ -11,35 +11,55 @@ namespace HelloWorldApplication //namespace声明命名空间,包含一个hello
         /* main函数 */
         static void Main(string[] args)//main函数是C#的接入口！
         {
-            /* 我的第一个 C# 程序 */
-
-
-            Db.fd_task task = new Db.fd_task
-            {
-                TaskId = 1001,
-                Count = 1,
-                State = 1,
-            };
-
-            byte[] buffer = task.ToByteArray();
-
-            Db.fd_task p = Db.fd_task.Parser.ParseFrom(buffer);
-
+            /* 我的第一个 C# 程序 */
             CDMRC oArc4 = new CDMRC();
             oArc4.SetKey("1234");
 
-            oArc4.Encrypt(buffer, 0, buffer, 0, buffer.Length);
-            oArc4.Decrypt(buffer, 0, buffer, 0, buffer.Length);
+            string strInput = "data\0d";
 
-            string strOut = System.Text.Encoding.ASCII.GetString(buffer);
+            byte[] InArray = System.Text.Encoding.ASCII.GetBytes(strInput);
+            byte[] OutArray = System.Text.Encoding.ASCII.GetBytes(strInput);
+
+            oArc4.Encrypt(InArray, 0, OutArray, 0, InArray.Length);
+            oArc4.Decrypt(OutArray, 0, InArray, 0, OutArray.Length);
+
+            string strOut = System.Text.Encoding.ASCII.GetString(InArray);
             Console.WriteLine(strOut);
 
-            oArc4.Encrypt(buffer);
-            oArc4.Decrypt(buffer);
+            oArc4.Encrypt(InArray);
+            oArc4.Decrypt(InArray);
 
-            Db.fd_task p2 = Db.fd_task.Parser.ParseFrom(buffer);
+            string strOut2 = System.Text.Encoding.ASCII.GetString(InArray);
 
-            Console.WriteLine(p2.ToString());
+            Console.WriteLine(strOut2);
+
+
+            //Db.fd_task task = new Db.fd_task
+            //{
+            //    TaskId = 1001,
+            //    Count = 1,
+            //    State = 1,
+            //};
+
+            //byte[] buffer = task.ToByteArray();
+
+            //Db.fd_task p = Db.fd_task.Parser.ParseFrom(buffer);
+
+            //CDMRC oArc4 = new CDMRC();
+            //oArc4.SetKey("1234");
+
+            //oArc4.Encrypt(buffer, 0, buffer, 0, buffer.Length);
+            //oArc4.Decrypt(buffer, 0, buffer, 0, buffer.Length);
+
+            //string strOut = System.Text.Encoding.ASCII.GetString(buffer);
+            //Console.WriteLine(strOut);
+
+            //oArc4.Encrypt(buffer);
+            //oArc4.Decrypt(buffer);
+
+            //Db.fd_task p2 = Db.fd_task.Parser.ParseFrom(buffer);
+
+            //Console.WriteLine(p2.ToString());
         }
     }
 }
